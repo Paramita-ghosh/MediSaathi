40624b1d911e1ec74760b219139e32773516a4da
nishi885 <nishi.20243187@mnnit.ac.in>
2026-05-31 20:34:18 +0530

    Update chatRoutes.js

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,13 +1,3 @@
-// import express from 'express';
-// import { generateChatResponse } from '../controllers/chatController.js';
-// import { protect } from '../middleware/authMiddleware.js';
-
-// const router = express.Router();
-
-// router.post('/generate', protect, generateChatResponse);
-
-// export default router;
-
 
 import express from "express";
 import { generateChatResponse } from "../controllers/chatController.js";
@@ -16,7 +6,7 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// Use only one auth middleware to avoid double auth conflicts
+
 router.post("/generate", generateChatResponse);
 
 export default router;
